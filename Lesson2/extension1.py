55b9cdab272f823a03647e1c4454dc85fce52dcb
Zachary Smith <156365790+zsmithc2c@users.noreply.github.com>
2025-06-02 14:41:30 +0000

    lesson 2 extension

---
+++ Lesson2/extension1.py
@@ -0,0 +1,52 @@
+###
+# Food Stand Budget Planner
+###
+
+# Our business name
+business_name = "Egg Express"
+
+# Menu Prices
+# We like to keep it simple. All our items are whole dollars. No change required!
+plain_eggs_price = 2
+cheesy_eggs_price = 3
+deluxe_eggs_price = 5
+
+# Sales projections
+# Lots of work has gone into projecting how many of each menu item we will sell.
+plain_eggs_sold = 240
+cheesy_eggs_sold = 1050
+deluxe_eggs_sold = 1350
+
+# Revenue calculations
+# TODO: Update this comment with the formula to calculate revenue.
+
+# TODO: Calculate revenue for each menu item's sales and store each in a variable
+
+###
+# Report Output
+###
+
+# Remember to print the business name at the top!
+print(business_name)
+print()  # This adds a blank line. It looks better with the extra space
+
+# Print the list of prices for each menu item
+print("Prices")
+print("------")
+
+print(f"Plain Eggs: ${plain_eggs_price}")
+print(f"Cheesy Eggs: ${cheesy_eggs_price}")
+print(f"Deluxe Eggs: ${deluxe_eggs_price}")
+
+print()  # Again, the blank line looks good.
+
+# Print the revenue estimate for each menu item
+print("Estimated Monthly Revenue")
+print("-------------------------")
+
+# TODO: Print out the Monthly Revenue for each item once we have performed the calculations
+#       I think we should use the same format as was used for the list prices
+
+
+# TODO: Add up the revenue of each item to determine the total monthly revenue.
+#       Store that in a variable and then print it in the report.
